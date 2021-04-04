dd0802ded767ba75d8a47a0a9bc4d41e257a7c2f
Phil Crossland <phil@thirdwheelstudios.com>
2021-04-04 16:50:53 +0100

    End of lesson 2 - Writing a unit test with Jest

---
+++ tests/unit/AppHeader.spec.js
@@ -0,0 +1,20 @@
+import AppHeader from '@/components/AppHeader'
+import { mount } from '@vue/test-utils'
+
+describe('AppHeader', () => {
+  test('If user is not logged in, do not show logout button', async () => {
+    const wrapper = mount(AppHeader)
+    wrapper.setData({ loggedIn: false })
+
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('button').isVisible()).toBe(false)
+  })
+
+  test('If user is logged in, show logout button', async () => {
+    const wrapper = mount(AppHeader)
+    wrapper.setData({ loggedIn: true })
+
+    await wrapper.vm.$nextTick()
+    expect(wrapper.find('button').isVisible()).toBe(true)
+  })
+})
